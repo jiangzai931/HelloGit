f1bd36f02735c9b50ae5138a6ec60f0a1087147c
jiangzai931 <1096593210@qq.com>
2021-01-12 14:37:18 +0800

    添加文件

---
+++ HelloGit/src/main/java/jz/git/HelloGit.java
@@ -0,0 +1,12 @@
+package jz.git;
+
+/**
+ * @version 1.0
+ * @author: JiangZai
+ * @date: 2021/1/12 8:40
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
